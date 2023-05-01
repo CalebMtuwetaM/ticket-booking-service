e651fd45297da82cf5301db4f1971212d5e32743
Caleb <73331147+westillest@users.noreply.github.com>
2023-05-01 19:56:24 +0000

    added the edit_event function

--- app.py
+++ app.py
@@ -100,3 +100,43 @@ def list_events():
         events.append(event)
     print (events)
 
+# a function to edit the event details 
+def edit_event(event_id, name=None, start_date=None, end_date=None, venue=None):
+    # Ensure that the event_id is not None
+    if event_id is None:
+        raise ValueError("event_id cannot be None")
+
+    # Check if the event exists in the database
+    cursor.execute("SELECT * FROM events WHERE event_id = %s", (event_id,))
+    result = cursor.fetchone()
+    if result is None:
+        raise ValueError("Event with event_id %s does not exist" % event_id)
+
+    # Update the event details
+    if name is not None:
+        cursor.execute("UPDATE events SET event_name = %s WHERE event_id = %s", (name, event_id))
+    if start_date is not None:
+        cursor.execute("UPDATE events SET start_date = %s WHERE event_id = %s", (start_date, event_id))
+    if end_date is not None:
+        cursor.execute("UPDATE events SET end_date = %s WHERE event_id = %s", (end_date, event_id))
+    if venue is not None:
+        cursor.execute("UPDATE events SET venue = %s WHERE event_id = %s", (venue, event_id))
+
+    # Commit the changes to the database
+    connection.commit()
+
+    # Retrieve the updated event from the database
+    cursor.execute("SELECT * FROM events WHERE event_id = %s", (event_id,))
+    result = cursor.fetchone()
+
+    updated_event = {
+        "event_id": result[0],
+        "event_name": result[1],
+        "start_date": result[2],
+        "end_date": result[3],
+        "venue": result[4]
+    }
+
+    return updated_event
+
+     
--- run_app.py
+++ run_app.py
@@ -7,4 +7,7 @@ import app
 #app.delete_event("ab19fd01-e59e-4a67-8ef9-d746187c16d3")
 
 # how to list all the events 
-app.list_events()
+#app.list_events()
+
+# how to edit the event details
+app.edit_event("6182e1f1-ca71-408d-b6fe-637aeb36de95","easter","2023-06-15 18:30:00","2023-09-01 20:00:00","Church")
