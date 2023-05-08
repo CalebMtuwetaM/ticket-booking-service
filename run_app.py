02d0b0231c284f69b3a1a233ef15f2599ad5bfc1
Caleb <73331147+westillest@users.noreply.github.com>
2023-05-08 05:00:34 +0000

    added test for app

--- run_app.py
+++ run_app.py
@@ -10,4 +10,4 @@ import app
 #app.list_events()
 
 # how to edit the event details
-app.edit_event("6182e1f1-ca71-408d-b6fe-637aeb36de95","easter","2023-06-15 18:30:00","2023-09-01 20:00:00","Church")
+#app.edit_event("6182e1f1-ca71-408d-b6fe-637aeb36de95","easter","2023-06-15 18:30:00","2023-09-01 20:00:00","Church")
---
+++ test_app.py
@@ -0,0 +1,60 @@
+# import all necessary modules 
+import os
+import unittest
+import MySQLdb
+from dotenv import load_dotenv
+import app
+#import pytest 
+
+# Load environment variables from .env file
+load_dotenv()
+#@pytest_fixture
+class TestEvents(unittest.TestCase): 
+    def setUp(self):
+        # Set up a connection to the database
+        self.connection = MySQLdb.connect(
+            host=os.getenv("hostname"),
+            user=os.getenv("username"),
+            passwd=os.getenv("password"),
+            db=os.getenv("database"),
+            ssl={
+                "ssl_ca": "/etc/ssl/cert.pem"
+            }
+        )
+        
+        # test data 
+
+        #app.create_event("play games","2023-06-15 18:30:00","2023-09-01 20:00:00","home")
+
+
+
+        # Create a cursor object for executing SQL queries
+        self.cursor = self.connection.cursor()
+
+    def tearDown(self):
+        # Close the database connection after each test
+        self.connection.close()
+        # request.addfinalizer()
+
+    def test_create_event(self):
+        # Happy path test for create_event function 
+        is_created = False
+        app.create_event("play games","2023-06-15 18:30:00","2023-09-01 20:00:00","home")
+        is_created = True
+        if is_created == False:
+            self.assertRaises(ValueError)
+
+    def test_delete_event(self):
+        # TODO: write test for delete_event function
+        pass
+
+    def test_list_events(self):
+        # TODO: write test for list_events function
+        pass
+
+    def test_edit_event(self):
+        # TODO: write test for edit_event function
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
